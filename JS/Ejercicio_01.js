106adbeb3ceb8618af0facb4c709a1ffa7fa325c
Edwin Hernandez Campos <edwincampos803@gmail.com>
2024-10-15 23:59:48 -0600

    Practica 4
    Se agregaron cambios y se agrego otra parte del codigodel ejercicio 2

--- JS/Ejercicio_01.js
+++ JS/Ejercicio_01.js
@@ -4,19 +4,19 @@
  */
 // EJERCICIO 01 : Declaración de Variables
 // a) Var
-var miNombre = "Jesús";
+var miNombre = "Edwin";
 var misApellidos;
 var miEdad = 19;
 // El objeto de console nos permite envair datos a la terminal y poder visualizar el valor de una variable o resultado de una función o método
 console.warn("--- Declaración de variables usando: VAR");
 console.log("Intentando leer las variables: ",miNombre,misApellidos);
-misApellidos = "Domínguez Ramírez";
+misApellidos = "Hernandez Campos";
 console.log("Ya que ambas fueron inicializadas vuelvo a intentar leerlas",miNombre,misApellidos);
 // Una variable puede cambiar en el proceso de ejecución del programa.
 // b) Const
 console.warn("--- Declaración de variables del tipo constante");
 const miUniversidad = "UT Xicotepec";
-const miMatricula = 230028;
+const miMatricula = 230425;
 console.log("Hola, ",miNombre," ",misApellidos," se que estudias actualmente en: ",miUniversidad, " asignaron la matricula: ",miMatricula, " y tienes una edad de: ", miEdad," años.");
 // Para saber el tipo de dato que tiene una variable ocupamos la función typeOf()
 console.log("Analizando los datos puedo deducir que: ")
@@ -26,7 +26,7 @@ console.log("miUniversidad es del tipo: ", typeof(miUniversidad));
 console.log("miMatricula es del tipo: ", typeof(miMatricula));
 console.log("miEdad es del tipo: ", typeof(miEdad));
 // c) Let
-let miFechaNacimiento = "2005-05-08";
+let miFechaNacimiento = "2004-11-23";
 let miColorFavorito;
 console.warn("--- Declaración de Variable Locales usando: LET")
 console.log("Genial, te voy conociendo mejor, ahora sé que tu naciste el: ", miFechaNacimiento," y tu colorFavorito es: mmmmmmm dejame pensar....");
